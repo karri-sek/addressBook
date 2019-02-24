a5f5bb120c0510b3597d18c49ac47fe4db015f28
Sekhar <sekhar@daisie.com>
2019-02-24 10:54:40 +0000

    static import Assert package adding test to check file existence this test will get failed as there is no implementation exists at the implementation class

--- src/test/java/com/gumtree/addressbook/readingStrategy/CSVFileReadingStrategyTest.java
+++ src/test/java/com/gumtree/addressbook/readingStrategy/CSVFileReadingStrategyTest.java
@@ -2,12 +2,17 @@ package com.gumtree.addressbook.readingStrategy;
 
 import com.gumtree.addressbook.readingStrategy.impl.CSVFileReadingStrategy;
 import org.junit.Test;
-import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.*;
 public class CSVFileReadingStrategyTest {
 
     @Test
     public void testNoFileExistence(){
         CSVFileReadingStrategy csvReading = new CSVFileReadingStrategy();
         assertFalse(csvReading.isFileExists("src/test/resources/nofile.csv"));
+    }
+    @Test
+    public void fileShouldExist(){
+        CSVFileReadingStrategy csvReading = new CSVFileReadingStrategy();
+        assertTrue(csvReading.isFileExists("src/test/resources/addressBook.csv"));
     }
 }
