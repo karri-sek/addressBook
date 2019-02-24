2ae1cd4190f33ff02d4527f098e09da7e9197d05
sekhar <babusek@gmail.com>
2019-02-24 20:45:36 +0000

    EntryPoint classes Adding entry point class to this application called ApplicationEntry copied addessBook csv file to resources folder injected the application dependencies from the applicationEntry class Added FileUtils class to read line and map to entity

---
+++ src/main/java/com/gumtree/addressbook/ApplicationEntry.java
@@ -0,0 +1,35 @@
+package com.gumtree.addressbook;
+
+import com.gumtree.addressbook.entities.Address;
+import com.gumtree.addressbook.readingStrategy.ReadingContext;
+import com.gumtree.addressbook.readingStrategy.impl.CSVFileReadingStrategy;
+import com.gumtree.addressbook.service.AddressBookService;
+import com.gumtree.addressbook.service.AddressBookServiceImpl;
+import com.gumtree.addressbook.utils.FileUtils;
+
+public class ApplicationEntry {
+
+
+    public static ReadingContext readingContext = new ReadingContext();
+    public static String filepath = "src/resources/addressBook.csv";
+    public static Address address;
+    public AddressBookService addressBookService;
+
+    public void setAddressBookService(AddressBookService addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+    public static  void main(String[] args){
+
+        readingContext.setFileReadingStrategy(new CSVFileReadingStrategy());
+        FileUtils fileUtlis = new FileUtils(readingContext);
+        address = new Address();
+        address.setAddressBookRecords(fileUtlis.getListOfAddressEntities(filepath));
+        ApplicationEntry applicationEntry = new ApplicationEntry();
+        applicationEntry.setAddressBookService(new AddressBookServiceImpl(address));
+        applicationEntry.printAllCallsStatstics();
+    }
+
+    public void printAllCallsStatstics(){
+        System.out.println("No of males from the addressBook:"+addressBookService.getMaleGenderCount());
+    }
+}
