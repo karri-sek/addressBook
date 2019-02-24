88de82451402c79883112e3e9920f9aa519c2d87
sekhar <babusek@gmail.com>
2019-02-24 18:09:34 +0000

    adding tests related to compare the ages by date of birth

--- src/test/java/com/gumtree/addressbook/entities/AddressTest.java
+++ src/test/java/com/gumtree/addressbook/entities/AddressTest.java
@@ -66,4 +66,33 @@ public class AddressTest {
         assertFalse(firstAddress.equals(secondAddress));
         assertNotEquals(firstAddress.hashCode(), secondAddress.hashCode());
     }
+    @Test
+    public void BillShouldOlderThenNillAndReturnsNegative() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+        LocalDate firstAddressDOB = LocalDate.parse("27/10/87", formatter);
+        Address firstAddress = new Address("Bill", "Queen", Gender.Male, firstAddressDOB);
+        LocalDate secondAddressDOB = LocalDate.parse("16/03/97", formatter);
+        Address secondAddress = new Address("Nill", "Queen", Gender.Male, secondAddressDOB);
+        assertTrue(Address.compareByDateOfBirth(firstAddress,secondAddress)<0);
+    }
+
+    @Test
+    public void JohnShouldYoungerThenSamAndReturnsPositive() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+        LocalDate firstAddressDOB = LocalDate.parse("27/10/87", formatter);
+        Address firstAddress = new Address("Sam", "muttok", Gender.Male, firstAddressDOB);
+        LocalDate secondAddressDOB = LocalDate.parse("16/03/97", formatter);
+        Address secondAddress = new Address("John", "Shepard", Gender.Male, secondAddressDOB);
+        assertTrue(Address.compareByDateOfBirth(firstAddress,secondAddress)<0);
+    }
+
+    @Test
+    public void ElliotAndPaulAreAtSameAge() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+        LocalDate firstAddressDOB = LocalDate.parse("27/10/87", formatter);
+        Address firstAddress = new Address("Elliot", "muttok", Gender.Male, firstAddressDOB);
+        LocalDate secondAddressDOB = LocalDate.parse("27/10/87", formatter);
+        Address secondAddress = new Address("Paul", "Shepard", Gender.Male, secondAddressDOB);
+        assertTrue(Address.compareByDateOfBirth(firstAddress,secondAddress)==0);
+    }
 }
