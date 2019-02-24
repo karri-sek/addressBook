c5cca0ed79b151088af6c3f1c242d09ed98b7898
Sekhar <sekhar@daisie.com>
2019-02-24 12:39:46 +0000

    Renamed the AddressBookEntity class to AddressBook

--- src/main/java/com/gumtree/addressbook/readingStrategy/entities/AdressBook.java
+++ src/main/java/com/gumtree/addressbook/readingStrategy/entities/AdressBook.java
@@ -4,9 +4,9 @@ import com.gumtree.addressbook.readingStrategy.enums.Gender;
 
 import java.time.LocalDate;
 
-public class AdressBookEntity extends Entity {
+public class AdressBook extends Entity {
     private LocalDate dateOfBirth;
-    public AdressBookEntity(String firstName, String lastName, Gender gender, LocalDate dateOfBirth){
+    public AdressBook(String firstName, String lastName, Gender gender, LocalDate dateOfBirth){
       this.firstName = firstName;
       this.lastName = lastName;
       this.dateOfBirth = dateOfBirth;
